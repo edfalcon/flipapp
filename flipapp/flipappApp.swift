cdc518d4e7a19ef0ee737138f0d92008a3c5df7f
Ed Falcon <ed@thefalcons.xyz>
2024-05-09 21:46:58 -0500

    Initial Commit

---
+++ flipapp/flipappApp.swift
@@ -0,0 +1,17 @@
+//
+//  flipappApp.swift
+//  flipapp
+//
+//  Created by Edward Falcon on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct flipappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
