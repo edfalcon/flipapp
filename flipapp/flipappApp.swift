54217e8e5bf6a84edf8b3b4b0e68049046dbab88
Ed Falcon <ed@thefalcons.xyz>
2024-05-17 07:49:55 -0500

    abc

--- flipapp/flipappApp.swift
+++ flipapp/flipappApp.swift
@@ -1,17 +1,52 @@
 //
-//  flipappApp.swift
+//  CreateFlashcardView.swift
 //  flipapp
 //
-//  Created by Edward Falcon on 5/9/24.
+//  Created by Edward Falcon on 5/16/24.
 //
 
 import SwiftUI
+import CoreData
 
-@main
-struct flipappApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+struct CreateFlashcardView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @State private var question: String = ""
+    @State private var answer: String = ""
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("New Flashcard")) {
+                    TextField("Question", text: $question)
+                    TextField("Answer", text: $answer)
+                }
+                Button(action: addFlashcard) {
+                    Text("Save Flashcard")
+                }
+            }
+            .navigationBarTitle("Create Flashcard")
         }
+    }
+
+    private func addFlashcard() {
+        let newFlashcard = Flashcard(context: viewContext)
+        newFlashcard.question = question
+        newFlashcard.answer = answer
+        newFlashcard.isKnown = false
+        newFlashcard.lastReviewed = Date()
+        
+        do {
+            try viewContext.save()
+            question = ""
+            answer = ""
+        } catch {
+            print("Failed to save flashcard: \(error.localizedDescription)")
+        }
+    }
+}
+
+struct CreateFlashcardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateFlashcardView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
