50b28b560daf81a31af8cad1bb857bb8ca13f653
milanthron <168153319+milanthron@users.noreply.github.com>
2024-05-16 23:14:21 -0500

    Update CreateFlashcardView.swift

--- CreateFlashcardView.swift
+++ CreateFlashcardView.swift
@@ -6,13 +6,48 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct CreateFlashcardView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @State private var question: String = ""
+    @State private var answer: String = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Section(header: Text("New Flashcard")) {
+                    TextField("Question", text: $question)
+                    TextField("Answer", text: $answer)
+                }
+                Button(action: addFlashcard) {
+                    Text("Save Flashcard")
+                }
+            }
+            .navigationBarTitle("Create Flashcard")
+        }
+    }
+
+    private func addFlashcard() {
+        let newFlashcard = Flashcard(context: viewContext)
+        newFlashcard.question = question
+        newFlashcard.answer = answer
+        newFlashcard.isKnown = false
+        newFlashcard.lastReviewed = Date()
+        
+        do {
+            try viewContext.save()
+            question = ""
+            answer = ""
+        } catch {
+            print("Failed to save flashcard: \(error.localizedDescription)")
+        }
     }
 }
 
-#Preview {
-    CreateFlashcardView()
+struct CreateFlashcardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateFlashcardView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
 }
+
